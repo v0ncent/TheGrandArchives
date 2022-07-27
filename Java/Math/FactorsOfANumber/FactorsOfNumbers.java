0a54e81b070829a38ec7cb150874ab8b046933e5
v0ncent <banksvincent5@gmail.com>
2022-07-27 15:18:18 -0500

    Created Factors of integer algorithm

--- Java/Math/FactorsOfANumber/FactorsOfNumbers.java
+++ Java/Math/FactorsOfANumber/FactorsOfNumbers.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//Authored by v0ncent
 public class FactorsOfNumbers {
     //Below are two algorithms that can be used to get an array of its factors from a given number
     static List<Integer> getFactors(int number){
+
         List<Integer> factors = new ArrayList<>();
+
         if(number < 0){ //if number is negative
             for (int i = number; i<= Math.abs(number); i++){
                 if(i==0){ //skip so we dont divide by 0
@@ -13,9 +16,9 @@ public class FactorsOfNumbers {
                     factors.add(i);
                 }
             }
-        } else {
-            for (int i = 1; i<=number; i++){
-                if(number % i == 0){
+        } else { //if number is positive
+            for (int i = 1; i<=number; i++){ //loop from 1 - number
+                if(number % i == 0){ //if mod returns 0 it is a factor
                     factors.add(i);
                 }
             }
