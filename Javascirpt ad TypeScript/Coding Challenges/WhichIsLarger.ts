f4405b5c37ff035b7756ac35d5f7667c95531dbc
v0ncent <banksvincent5@gmail.com>
2022-08-06 22:50:57 -0500

    Created arrayChallenges, creates js and ts package

---
+++ Javascirpt ad TypeScript/Coding Challenges/WhichIsLarger.ts
@@ -0,0 +1,20 @@
+/*
+Your function will be passed two functions, f and g, that don't take any parameters. 
+Your function has to call them, and return a string which indicates which function returned the larger number.
+
+If f returns the larger number, return the string f.
+If g returns the larger number, return the string g.
+If the functions return the same number, return the string neither.
+ */
+
+//the below algorithm solves the above problem
+
+function wichIsLarger(f:Function,g:Function){
+    if (g() < f()) {
+        return "f";
+    }
+    if (f() < g()) {
+        return "g";
+    }
+    return "neither";
+}
