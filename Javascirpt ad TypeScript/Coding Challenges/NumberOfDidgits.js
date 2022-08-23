ab20e13241a07f0e243c4672d38e7cf91d2085e6
v0ncent <banksvincent5@gmail.com>
2022-08-23 15:11:06 -0500

    Added more python challenges and JS and TS algs

---
+++ Javascirpt ad TypeScript/Coding Challenges/NumberOfDidgits.js
@@ -0,0 +1,7 @@
+/**
+ * Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+ */
+//the below algorithm solves the above challenge
+const getNumberOfDidgets = (num) => {
+    return Math.abs(num).toString().length;
+};
